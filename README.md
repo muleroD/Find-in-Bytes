83868f4bab816b02bb17725e7c00710247c76b47
Richard Mulero <richard.mulero@fcamara.com.br>
2022-04-26 23:39:42 -0300

    feat: criando classe para leitura do conteúdo do arquivo

--- README.md
+++ README.md
@@ -1,23 +1,40 @@
 # Find n' Bytes
-A proposta desse repositório é ter como exemplo, como consultar texto dentro de bytes utilizando funções do Oracle/Oracle Text usando Java com Spring.
+
+A proposta desse repositório é ter como exemplo, como consultar texto dentro de bytes utilizando funções do
+Oracle/Oracle Text usando Java com Spring.
 
 ## Motivação
-Um tempo atrás recebi um desafio bastante curioso, e resolvi criar esse repositório em memória dele. A proposta do desafio era a seguinte:
+
+Um tempo atrás recebi um desafio bastante curioso, e resolvi criar esse repositório em memória dele. A proposta do
+desafio era a seguinte:
 
 1. O usuário pode fazer o upload de um arquivo com as seguintes extensões: csv, pdf, txt, doc, docx, xls, xlsx, xml;
 2. O usuário pode baixar qualquer arquivo enviado;
 3. Deve haver uma forma de pesquisar dentro do arquivo com uma palavra e retornar um trecho em que ela se encontra.
 
 ## Restrições
-Deveria ser uma API Rest utilizando Java com Spring e se conectando ao banco de dados Oracle.
-Não deve utilizar nenhum mecanismo de busca ou analise como por exemplo Elasticsearch, etc.
+
+Uma API Rest utilizando Java com Spring e se conectando ao banco de dados Oracle.
+Não utilizar nenhum mecanismo de busca ou analise como, Elasticsearch, etc.
 
 ## Get Started
 
 ### Primeiros Passos
-Resolvi começar pelo "simples", receber o arquivo do usuário e salvar no banco como BLOB para que eu consiga baixar novamente, dessa forma não preciso manter o arquivo em nenhum diretório ou algo do tipo.
 
+* Docker
 
-## Documentação da API 
+### Modelagem 
+
+Resolvi começar pelo "simples", receber o arquivo do usuário e salvar no banco como BLOB para que eu consiga baixar
+novamente, dessa forma não preciso manter o arquivo em nenhum diretório ou algo do tipo.
+
+Vamos criar uma tabela chamada Documento que inicialmente vai ter as seguintes colunas:
+
+- **ID** - identificador com valor gerado automaticamente;
+- **NOME** - nome completo do arquivo;
+- **ARQUIVO** - bytes do arquivo;
+- **CONTEUDO** - conteúdo de dentro do arquivo.
+
+## Documentação da API
 
 http://localhost:8080/swagger-ui/index.html
---
+++ src/main/java/br/com/mulero/findnbytes/util/FileReaderUtils.java
@@ -0,0 +1,78 @@
+package br.com.mulero.findnbytes.util;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+
+import static org.apache.commons.io.FilenameUtils.getExtension;
+
+@Slf4j
+public class FileReaderUtils {
+
+    public static byte[] getBytes(MultipartFile file) {
+        try {
+            return file.getBytes();
+        } catch (IOException ioException) {
+            throw new IllegalArgumentException("Erro ler bytes do arquivo: " + ioException.getMessage());
+        }
+    }
+
+    public static String getContent(MultipartFile file) {
+        try {
+            InputStream inputStream = file.getInputStream();
+            String ext = getExtension(file.getOriginalFilename());
+
+            switch (ext) {
+                case "txt":
+                    return getContentFromSimplesFile(inputStream);
+
+                case "doc":
+                    return getContentFromDoc(inputStream);
+
+                case "docx":
+                    return getContentFromDocx(inputStream);
+
+
+                default:
+                    throw new IllegalArgumentException("Extensão inválida: " + ext);
+            }
+        } catch (IOException ioException) {
+            log.error(ioException.getMessage());
+            throw new IllegalArgumentException("Erro ao ler conteúdo do arquivo: " + ioException.getMessage());
+        }
+    }
+
+    private static String getContentFromSimplesFile(InputStream inputStream) throws IOException {
+        StringWriter writer = new StringWriter();
+        String encoding = StandardCharsets.UTF_8.name();
+        IOUtils.copy(inputStream, writer, encoding);
+        return writer.toString();
+    }
+
+    private static String getContentFromDoc(InputStream inputStream) throws IOException {
+        WordExtractor extractor = new WordExtractor(new HWPFDocument(inputStream));
+        String content = extractor.getText();
+        extractor.close();
+        inputStream.close();
+        return content;
+
+    }
+
+    private static String getContentFromDocx(InputStream inputStream) throws IOException {
+        XWPFWordExtractor extractor = new XWPFWordExtractor(new XWPFDocument(inputStream));
+        String content = extractor.getText();
+        extractor.close();
+        inputStream.close();
+        return content;
+    }
+}
